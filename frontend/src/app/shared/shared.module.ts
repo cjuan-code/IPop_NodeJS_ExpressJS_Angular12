5e84ad42e09017d6711d76c69120d1657e69b820
cjuan-code <cjuaniestacio@gmail.com>
2021-10-04 11:40:28 +0200

    Added footer

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './layout/header.component';
+import { FooterComponent } from './layout/footer.component';
 import { ListItemsComponent } from './list_items/list-items.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     HeaderComponent,
+    FooterComponent,
     ListItemsComponent
   ],
   imports: [
@@ -15,6 +17,7 @@ import { RouterModule } from '@angular/router';
   ],
   exports: [
     HeaderComponent,
+    FooterComponent,
     ListItemsComponent
   ],
 })
