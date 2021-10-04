758ede207ea96633fa25a548b2fb975ec301d74d
cjuan-code <cjuaniestacio@gmail.com>
2021-10-04 15:22:22 +0200

    slugify category

--- backend/models/Category.js
+++ backend/models/Category.js
@@ -1,11 +1,25 @@
 const mongoose = require('mongoose');
 var uniqueValitador = require('mongoose-unique-validator');
+var slugf = require('slug');
 
 const CategorySchema = mongoose.Schema({
+    slug: {type: String, lowercase: true, unique: true},
     name: String,
     img: String
 });
 
 CategorySchema.plugin(uniqueValitador, {message: "already exists."});
+
+CategorySchema.pre('validate', function(next) {
+    if (!this.slug) {
+        this.slugify();
+    }
+
+    next();
+});
+
+CategorySchema.methods.slugify = function() {
+    this.slug = slugf(this.name) + '-segunda-mano';
+};  
 
 module.exports = mongoose.model('category', CategorySchema);
