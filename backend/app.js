20ef53a8265490f535dec0d0afd259462b35298a
cjuan-code <cjuaniestacio@gmail.com>
2021-11-22 22:36:42 +0100

    dockerize app with grafana and prometheus

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const client = require('prom-client');
 const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("cors");
@@ -7,6 +8,37 @@ const app = express();
 connectDB();
 
 const port = process.env.PORT || 4000;
+
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics({ timeout: 5000 });
+
+const counterHomeEndpoint = new client.Counter({
+    name: 'counterHomeEndpoint',
+    help: 'The total number of processed requests'
+});
+
+const counterMessageEndpoint = new client.Counter({
+    name: 'counterMessageEndpoint',
+    help: 'The total number of processed requests to get endpoint'
+});
+
+
+app.get('/', (req, res) => {
+    counterHomeEndpoint.inc();
+    res.send('Hello world\n');
+});
+
+app.get('/message', (req, res) => {
+    counterMessageEndpoint.inc();
+    res.send('Message endpoint\n');
+});
+
+
+app.get('/metrics', (req, res) => {
+   res.set('Content-Type', client.register.contentType);
+   res.end(client.register.metrics());
+});
 
 app.use(cors());
 
